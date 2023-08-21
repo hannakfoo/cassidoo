6a8dc3192c69c467e31bd4e2a2b64bd12c2b3577
Henk Jan Pluim <henkjan@kxdesign.nl>
2023-08-21 08:02:41 +0200

    added guessing game

---
+++ guessing-game/src/Program.cs
@@ -0,0 +1,26 @@
+﻿// Make a "guessing game" where there is a target number, and as the user makes guesses, the output returns higher or lower until the user is correct. 
+Console.WriteLine("*** Guessing Game ***");
+
+var isGuessed = false;
+var numberToGuess = new Random().Next(10);
+var times = 0;
+
+while (!isGuessed)
+{
+    var number = Console.ReadLine();
+    if (Convert.ToInt32(number) < numberToGuess)
+    {
+        Console.WriteLine("higher");
+    }
+    if (Convert.ToInt32(number) > numberToGuess)
+    {
+        Console.WriteLine("lower");
+    }
+    if (Convert.ToInt32(number) == numberToGuess)
+    {
+        isGuessed = true;
+    }
+    times++;
+}
+
+Console.WriteLine($"Correct! You won in: {times} guesses!");
